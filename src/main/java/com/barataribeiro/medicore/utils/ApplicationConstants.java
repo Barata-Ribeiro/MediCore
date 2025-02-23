3698460dbe65132e772f0dd1bae26ab0f65b2561
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-23 09:36:41 -0300

    fix: add missing constants to white list

--- src/main/java/com/barataribeiro/medicore/utils/ApplicationConstants.java
+++ src/main/java/com/barataribeiro/medicore/utils/ApplicationConstants.java
@@ -21,6 +21,8 @@ public class ApplicationConstants {
             "/",
             "/favicon.ico",
             "/resources/**",
+            "/static/**",
+            "/templates/**",
             "/css/**",
             "/js/**",
             "/assets/**",
