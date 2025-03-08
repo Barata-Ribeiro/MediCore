b634a17b979a96d9730f9e1be232cf40b46164f9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-08 11:02:37 -0300

    feat(user): change profile update method to PUT and update form action accordingly

--- src/main/java/com/barataribeiro/medicore/features/user/AppUserController.java
+++ src/main/java/com/barataribeiro/medicore/features/user/AppUserController.java
@@ -63,7 +63,7 @@ public class AppUserController {
         return "pages/dashboard/profile/profile";
     }
 
-    @PostMapping("/profile")
+    @PutMapping("/profile")
     @PreAuthorize("#username == authentication.name")
     public String updateProfile(@PathVariable String username, Model model,
                                 @Valid @ModelAttribute UpdateAppUserDto updateAppUserDto, BindingResult bindingResult) {
