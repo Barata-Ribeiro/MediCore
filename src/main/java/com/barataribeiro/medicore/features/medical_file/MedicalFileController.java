324a6bfe055f5b60d674da2f8f8e9ca0d55a7439
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-08 10:45:04 -0300

    feat(medical): add `UpdateMedicalFileDto` and enhance `MedicalFileController` for medical file updates

--- src/main/java/com/barataribeiro/medicore/features/medical_file/MedicalFileController.java
+++ src/main/java/com/barataribeiro/medicore/features/medical_file/MedicalFileController.java
@@ -1,5 +1,6 @@
 package com.barataribeiro.medicore.features.medical_file;
 
+import com.barataribeiro.medicore.features.medical_file.dtos.UpdateMedicalFileDto;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,18 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import static com.barataribeiro.medicore.utils.ApplicationConstants.PAGE_DESCRIPTION;
-import static com.barataribeiro.medicore.utils.ApplicationConstants.PAGE_TITLE;
+import static com.barataribeiro.medicore.utils.ApplicationConstants.*;
 
 @Controller
 @RequestMapping("/{username}/medical-history")
 @AllArgsConstructor(onConstructor_ = {@Autowired})
 public class MedicalFileController {
+    private final MedicalFileRepository medicalFileRepository;
+
     @GetMapping
     @PreAuthorize("#username == authentication.name")
     public String getMedicalFile(Authentication authentication, @PathVariable String username, Model model) {
+        MedicalFile medicalFile = medicalFileRepository
+                .findByUser_Username(username)
+                .orElseThrow(() -> new RuntimeException("Medical File not found."));
+
         model.addAttribute(PAGE_TITLE, "Medical History");
         model.addAttribute(PAGE_DESCRIPTION, "View your medical history and add new medical records.");
+        model.addAttribute("medicalFile", medicalFile);
+        model.addAttribute(UPDATE_MEDICAL_FILE_DTO, new UpdateMedicalFileDto());
+        model.addAttribute("success", false);
         return "pages/dashboard/medical_file/index";
     }
 }
