c368a14738b842f7c7f1ddd189b3b661276a72fe
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-08 10:03:19 -0300

    refactor(medical): update repository methods for improved clarity and performance

--- src/main/java/com/barataribeiro/medicore/features/medical_file/MedicalFileRepository.java
+++ src/main/java/com/barataribeiro/medicore/features/medical_file/MedicalFileRepository.java
@@ -3,24 +3,18 @@ package com.barataribeiro.medicore.features.medical_file;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface MedicalFileRepository extends JpaRepository<MedicalFile, UUID> {
     @EntityGraph(attributePaths = {"user.profile"})
-    Optional<MedicalFile> findByUser_Username(String username);
+    MedicalFile findByUser_Username(String username);
 
-    @Query("SELECT " +
-            "COUNT(lp) + COUNT(cbc) + COUNT(g) + COUNT(vd) + COUNT(vb) + COUNT(uac) + COUNT(ua) " +
-            "FROM MedicalFile mf " +
-            "LEFT JOIN mf.lipidProfiles lp " +
-            "LEFT JOIN mf.completeBloodCounts cbc " +
-            "LEFT JOIN mf.glucoses g " +
-            "LEFT JOIN mf.vitaminDs vd " +
-            "LEFT JOIN mf.vitaminB12s vb " +
-            "LEFT JOIN mf.ureaAndCreatinines uac " +
-            "LEFT JOIN mf.uricAcids ua " +
-            "WHERE mf.user.username = :username")
-    Long countTotalExams(String username);
+    @Query("""
+           SELECT SIZE(mf.lipidProfiles) + SIZE(mf.ureaAndCreatinines) + SIZE(mf.vitaminDs) + SIZE(mf.vitaminB12s) + SIZE(mf.glucoses) + SIZE(mf.uricAcids) + SIZE(mf.completeBloodCounts)
+           FROM MedicalFile mf
+           WHERE mf.user.username = :username
+           """)
+    long getTotalExamsCount(@Param("username") String username);
 }
