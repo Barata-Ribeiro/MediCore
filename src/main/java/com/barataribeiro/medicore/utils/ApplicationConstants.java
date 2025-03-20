2f36a33387e38179284e29d40e441b77e9f445d6
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-20 06:24:36 -0300

    feat(ultrasensitive-tsh): implement controller for managing Ultrasensitive TSH profiles

--- src/main/java/com/barataribeiro/medicore/utils/ApplicationConstants.java
+++ src/main/java/com/barataribeiro/medicore/utils/ApplicationConstants.java
@@ -7,10 +7,11 @@ public class ApplicationConstants {
     public static final String NEW_CBC_DTO = "newCBCDto";
     public static final String NEW_GLUCOSE_DTO = "newGlucoseDto";
     public static final String NEW_LIPID_PROFILE_DTO = "newLipidProfileDto";
-    public static final String NEW_VITAMIN_B_TWELVE_PROFILE_DTO = "newVitaminBTwelveProfileDto";
-    public static final String NEW_VITAMIND_PROFILE_DTO = "newVitaminDProfileDto";
+    public static final String NEW_ULTRASENSITIVE_TSH_PROFILE_DTO = "newUltrasensitiveTSHProfileDto";
     public static final String NEW_UREA_AND_CREATININE_PROFILE_DTO = "newUreaAndCreatinineProfileDto";
     public static final String NEW_URIC_ACID_PROFILE_DTO = "newUricAcidProfileDto";
+    public static final String NEW_VITAMIND_PROFILE_DTO = "newVitaminDProfileDto";
+    public static final String NEW_VITAMIN_B_TWELVE_PROFILE_DTO = "newVitaminBTwelveProfileDto";
     public static final String REGISTRATION_DTO = "registrationDto";
     public static final String UPDATE_APP_USER_DTO = "updateAppUserDto";
     public static final String UPDATE_MEDICAL_FILE_DTO = "updateMedicalFileDto";
