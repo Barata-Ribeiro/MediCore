07ae6fab888db7c5ea4db9923535b62b365965fb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-03-27 09:05:33 -0300

    feat(medical-file): add query to fetch medical file with latest exam results for a user

--- src/main/java/com/barataribeiro/medicore/features/medical_file/MedicalFileRepository.java
+++ src/main/java/com/barataribeiro/medicore/features/medical_file/MedicalFileRepository.java
@@ -13,6 +13,29 @@ public interface MedicalFileRepository extends JpaRepository<MedicalFile, UUID> 
     Optional<MedicalFile> findByUser_Username(String username);
 
     @Query("""
+           SELECT DISTINCT mf FROM MedicalFile mf
+           LEFT JOIN FETCH mf.user u
+           LEFT JOIN FETCH mf.lipidProfiles lp
+           LEFT JOIN FETCH mf.completeBloodCounts cbc
+           LEFT JOIN FETCH mf.glucoses g
+           LEFT JOIN FETCH mf.vitaminDs vd
+           LEFT JOIN FETCH mf.vitaminBTwelves vb
+           LEFT JOIN FETCH mf.ureaAndCreatinines uac
+           LEFT JOIN FETCH mf.uricAcids ua
+           LEFT JOIN FETCH mf.ultrasensitiveTSHs utsh
+           WHERE mf.user.username = :username
+           AND (lp IS NULL OR lp.reportDate = (SELECT MAX(lp2.reportDate) FROM LipidProfile lp2 WHERE lp2.medicalFile = mf))
+           AND (cbc IS NULL OR cbc.reportDate = (SELECT MAX(cbc2.reportDate) FROM CompleteBloodCount cbc2 WHERE cbc2.medicalFile = mf))
+           AND (g IS NULL OR g.reportDate = (SELECT MAX(g2.reportDate) FROM Glucose g2 WHERE g2.medicalFile = mf))
+           AND (vd IS NULL OR vd.reportDate = (SELECT MAX(vd2.reportDate) FROM VitaminD vd2 WHERE vd2.medicalFile = mf))
+           AND (vb IS NULL OR vb.reportDate = (SELECT MAX(vb2.reportDate) FROM VitaminBTwelve vb2 WHERE vb2.medicalFile = mf))
+           AND (uac IS NULL OR uac.reportDate = (SELECT MAX(uac2.reportDate) FROM UreaAndCreatinine uac2 WHERE uac2.medicalFile = mf))
+           AND (ua IS NULL OR ua.reportDate = (SELECT MAX(ua2.reportDate) FROM UricAcid ua2 WHERE ua2.medicalFile = mf))
+           AND (utsh IS NULL OR utsh.reportDate = (SELECT MAX(utsh2.reportDate) FROM UltrasensitiveTSH utsh2 WHERE utsh2.medicalFile = mf))
+           """)
+    Optional<MedicalFile> findMedicalFileByUser_UsernameWithLatestExams(@Param("username") String username);
+
+    @Query("""
             SELECT SIZE(mf.lipidProfiles) +\s
                        SIZE(mf.ureaAndCreatinines) +\s
                        SIZE(mf.vitaminDs) +\s
