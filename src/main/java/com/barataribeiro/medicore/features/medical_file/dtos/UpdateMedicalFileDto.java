d8ee5f4e9bf9ed2d73a0ec196ad7065a664df0da
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-05-31 10:11:22 -0300

    refactor(medical-file): remove validation annotations from UpdateMedicalFileDto

--- src/main/java/com/barataribeiro/medicore/features/medical_file/dtos/UpdateMedicalFileDto.java
+++ src/main/java/com/barataribeiro/medicore/features/medical_file/dtos/UpdateMedicalFileDto.java
@@ -3,7 +3,6 @@ package com.barataribeiro.medicore.features.medical_file.dtos;
 import com.barataribeiro.medicore.features.medical_file.BloodType;
 import com.barataribeiro.medicore.features.medical_file.MedicalFile;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -15,28 +14,14 @@ import java.io.Serializable;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UpdateMedicalFileDto implements Serializable {
-    @NotBlank(message = "Blood Type must be selected.")
     private BloodType bloodType;
-
-    @NotBlank(message = "Allergies must not be blank.")
     private String allergies;
-
-    @NotBlank(message = "Diseases must not be blank.")
     private String diseases;
-
-    @NotBlank(message = "Medications must not be blank.")
     private String medications;
-
-    @NotBlank(message = "Weight must not be blank.")
     private Double weight;
-
-    @NotBlank(message = "Height must not be blank.")
     private Double height;
-
-    @NotBlank(message = "Emergency Contact Name must not be blank.")
     private String emergencyContactName;
 
-    @NotBlank(message = "Emergency Contact Phone must not be blank.")
     @Pattern(regexp = "^(\\\\+\\\\d{1,3}( )?)?((\\\\(\\\\d{1,3}\\\\))|\\\\d{1,3})[- .]?\\\\d{3,4}[- .]?\\\\d{4}$",
              message = "Emergency Contact Phone must be a valid phone number.")
     private String emergencyContactPhone;
