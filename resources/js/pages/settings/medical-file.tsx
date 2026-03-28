809d83ea46b2e095160133fef350501e1db5a305
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-28 09:23:03 -0300

    feat(medical-file): add Medical File link to sidebar and implement settings form

--- resources/js/pages/settings/medical-file.tsx
+++ resources/js/pages/settings/medical-file.tsx
@@ -1,17 +1,16 @@
+import MedicalFileManagerForm from '@/components/forms/settings/medical-file-manager.form';
 import { edit } from '@/routes/medical-file';
 import { Head } from '@inertiajs/react';
 import { Fragment } from 'react/jsx-runtime';
 
-// TODO: implement medical file type
-export default function MedicalFile({ file }: Readonly<{ file: any }>) {
+export default function MedicalFile() {
     return (
         <Fragment>
             <Head title="Medical file settings" />
 
             <h1 className="sr-only">Medical file settings</h1>
 
-            {/* TODO: implement medical file settings form */}
-            <pre>{JSON.stringify(file, null, 2)}</pre>
+            <MedicalFileManagerForm />
         </Fragment>
     );
 }
