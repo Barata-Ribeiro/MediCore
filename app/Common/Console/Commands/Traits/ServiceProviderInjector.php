893b6805e4a9fb983b3a34d6e250abcc62c9f459
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-29 08:41:36 -0300

    feat(service): add ServiceProviderInjector trait for dynamic code injection

---
+++ app/Common/Console/Commands/Traits/ServiceProviderInjector.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands\Traits;
+
+use App\Providers\AppServiceProvider;
+use ReflectionClass;
+
+trait ServiceProviderInjector
+{
+    public function injectCodeToRegisterMethod($appServiceProviderFile, $codeToAdd): void
+    {
+        $reflectionClass = new ReflectionClass(AppServiceProvider::class);
+        $reflectionMethod = $reflectionClass->getMethod('register');
+
+        $methodBody = file($appServiceProviderFile);
+
+        $endLine = $reflectionMethod->getEndLine() - 1;
+
+        array_splice($methodBody, $endLine, 0, $codeToAdd);
+        $modifiedCode = implode('', $methodBody);
+
+        file_put_contents($appServiceProviderFile, $modifiedCode);
+    }
+}
