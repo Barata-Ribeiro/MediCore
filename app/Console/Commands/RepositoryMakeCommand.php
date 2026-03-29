2a7cbb9a99e7fbe14634fc91edee8c548fffd03b
João M J Barata Ribeiro <jmj@barataribeiro.com>
2026-03-29 08:58:23 -0300

    Add dynamic service and repository generation features (#16)
    * feat(helpers): add boolean query builder and date range normalizer
    
    * feat(service): add ServiceProviderInjector trait for dynamic code injection
    
    * feat(service): create ServiceMakeCommand for dynamic service class generation
    
    * feat(service): add service.stub for dynamic service class generation
    
    * fix(move-folders): place Console folder in correct path
    
    * feat(repository): add RepositoryMakeCommand and repository.stub for dynamic repository class generation
    
    * feat(interface): add InterfaceMakeCommand and interface.stub for dynamic interface class generation

---
+++ app/Console/Commands/RepositoryMakeCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\Commands\Traits\ServiceProviderInjector;
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Facades\Artisan;
+
+class RepositoryMakeCommand extends GeneratorCommand
+{
+    use ServiceProviderInjector;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:repository {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new Repository class';
+
+    /**
+     * Execute the console command.
+     *
+     * @throws FileNotFoundException
+     */
+    public function handle(): ?bool
+    {
+        $codeToAdd = "\n\t\t\$this->app->bind(\n".
+            "\t\t\t\\App\\Interfaces\\".str_replace('/', '\\', $this->argument('name'))."Interface::class,\n".
+            "\t\t\t\\App\\Repositories\\".str_replace('/', '\\', $this->argument('name'))."::class\n".
+            "\t\t);\n";
+
+        $appServiceProviderFile = app_path('Providers/AppServiceProvider.php');
+
+        $this->injectCodeToRegisterMethod($appServiceProviderFile, $codeToAdd);
+
+        Artisan::call('make:interface', [
+            'name' => $this->argument('name').'Interface',
+        ]);
+
+        return parent::handle();
+    }
+
+    /**
+     * Get the stub file for the generator.
+     */
+    protected function getStub(): string
+    {
+        return __DIR__.'/stubs/repository.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     */
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return "{$rootNamespace}\\Repositories";
+    }
+
+    protected function buildClass($name): string
+    {
+        $class = parent::buildClass($name);
+        $interfaceFqn = $this->qualifyInterfaceFqn($name);
+
+        return str_replace('{{ interfaceFqn }}', $interfaceFqn, $class);
+    }
+
+    protected function qualifyInterfaceFqn(string $name): string
+    {
+        $rootNamespace = rtrim($this->rootNamespace(), '\\');
+        $relativeName = str_replace(['/', '\\'], '\\', $this->argument('name'));
+
+        return "{$rootNamespace}\\Interfaces\\{$relativeName}Interface";
+    }
+}
