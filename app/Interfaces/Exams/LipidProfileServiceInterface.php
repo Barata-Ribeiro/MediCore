f70e0189667b6cd4173f7f5a778cce92d425ef43
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-29 09:48:30 -0300

    feat(services): enhance getLipidProfileData method with filtering and pagination

--- app/Interfaces/Exams/LipidProfileServiceInterface.php
+++ app/Interfaces/Exams/LipidProfileServiceInterface.php
@@ -4,5 +4,5 @@ namespace App\Interfaces\Exams;
 
 interface LipidProfileServiceInterface
 {
-
+    public function getLipidProfileData(?int $perPage, ?string $sortBy, ?string $sortDir, ?string $search, ?array $filters): array;
 }
--- app/Services/Exams/LipidProfileService.php
+++ app/Services/Exams/LipidProfileService.php
@@ -2,9 +2,51 @@
 
 namespace App\Services\Exams;
 
+use App\Common\Helpers;
 use App\Interfaces\Exams\LipidProfileServiceInterface;
+use App\Models\Exams\LipidProfile;
+use Concurrency;
 
 class LipidProfileService implements LipidProfileServiceInterface
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function getLipidProfileData(?int $perPage, ?string $sortBy, ?string $sortDir, ?string $search, ?array $filters): array
+    {
+        $createdAtRange = $filters['created_at'] ?? [];
+        $reportDateRange = $filters['report_date'] ?? [];
 
+        [$createdAtStart, $createdAtEnd] = Helpers::getDateRange($createdAtRange);
+        [$reportDateStart, $reportDateEnd] = Helpers::getDateRange($reportDateRange);
+
+        $lipidProfile = LipidProfile::query()
+            ->select('lipid_profiles.*')
+            ->where('medical_file_id', auth()->user()->medicalFile->id)
+            ->when($createdAtRange, fn ($q) => $q->whereBetween('lipid_profiles.created_at', [$createdAtStart, $createdAtEnd]))
+            ->when($reportDateRange, fn ($q) => $q->whereBetween('lipid_profiles.report_date', [$reportDateStart, $reportDateEnd]))
+            ->when($search, fn ($q) => $q->where(function ($q) use ($search) {
+                $q->whereLike('total_cholesterol', "%{$search}%")
+                    ->orWhereLike('hdl_cholesterol', "%{$search}%")
+                    ->orWhereLike('ldl_cholesterol', "%{$search}%")
+                    ->orWhereLike('vldl_cholesterol', "%{$search}%")
+                    ->orWhereLike('triglycerides', "%{$search}%");
+            }))
+            ->orderBy($sortBy, $sortDir)
+            ->paginate($perPage)
+            ->withQueryString();
+
+        $chartData = LipidProfile::query()
+            ->selectRaw('DATE(report_date) as date, total_cholesterol, hdl_cholesterol, ldl_cholesterol, vldl_cholesterol, triglycerides, (total_cholesterol / hdl_cholesterol) as ratio, count(*) as count')
+            ->where('medical_file_id', auth()->user()->medicalFile->id)
+            ->groupBy('date')
+            ->orderBy('date')
+            ->limit(5)
+            ->get();
+
+        return Concurrency::run([
+            fn () => $lipidProfile,
+            fn () => $chartData,
+        ]);
+    }
 }
