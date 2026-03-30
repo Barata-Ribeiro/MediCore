744543b27cf8052361cfd6715ed95d2b4c76244d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-30 16:08:48 -0300

    feat(blood-count): implement CompleteBloodCountController and add routes for complete blood count exam

--- routes/exams.php
+++ routes/exams.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Exams\CompleteBloodCountController;
 use App\Http\Controllers\Exams\LipidProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('exams')->group(function () {
     Route::resource('lipid-profile', LipidProfileController::class);
+    Route::resource('complete-blood-count', CompleteBloodCountController::class);
 });
