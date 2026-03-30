01aeb61d30dd543b73dfa083608863bddb1eed41
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-30 11:03:59 -0300

    feat(copy-button); add DropdownMenuCopyButton component for clipboard functionality

---
+++ resources/js/components/common/dropdown-menu-copy-button.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@/components/ui/button';
+import { useClipboard } from '@/hooks/use-clipboard';
+import { ClipboardCheckIcon, ClipboardIcon } from 'lucide-react';
+import type { ReactNode } from 'react';
+import { toast } from 'sonner';
+
+interface RawCopyButtonProps {
+    content: unknown;
+    children: ReactNode;
+}
+
+export default function DropdownMenuCopyButton({ content, children }: Readonly<RawCopyButtonProps>) {
+    const [copiedText, copy] = useClipboard();
+
+    const isContentInClipboard = copiedText === content;
+
+    return (
+        <Button
+            type="button"
+            variant="ghost"
+            size="sm"
+            className="w-full justify-start px-2"
+            disabled={isContentInClipboard}
+            onClick={() =>
+                copy(JSON.stringify(content)).then(() => toast.info('Copied to clipboard!', { duration: 2000 }))
+            }
+        >
+            {isContentInClipboard ? (
+                <ClipboardCheckIcon aria-hidden size={14} />
+            ) : (
+                <ClipboardIcon aria-hidden size={14} />
+            )}
+            {children}
+        </Button>
+    );
+}
