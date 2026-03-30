0ff809920f44e30c020820004e3d4af1628e80e3
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-03-30 15:51:40 -0300

    docs(ide-helpers): enhance LipidProfile model with additional properties and query methods

--- app/Models/Exams/LipidProfile.php
+++ app/Models/Exams/LipidProfile.php
@@ -3,17 +3,38 @@
 namespace App\Models\Exams;
 
 use App\Models\MedicalFile;
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Touches;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
- * @property-read MedicalFile|null $medicalFile
+ * @property int $id
+ * @property float $total_cholesterol Total Cholesterol
+ * @property float $hdl_cholesterol High-Density Lipoprotein Cholesterol
+ * @property float $ldl_cholesterol Low-Density Lipoprotein Cholesterol
+ * @property float $vldl_cholesterol Very Low-Density Lipoprotein Cholesterol
+ * @property float $triglycerides Triglycerides
+ * @property CarbonImmutable $report_date Date of the lipid profile report
+ * @property CarbonImmutable|null $created_at
+ * @property CarbonImmutable|null $updated_at
+ * @property int $medical_file_id
+ * @property-read MedicalFile $medicalFile
  *
  * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereHdlCholesterol($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereLdlCholesterol($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereMedicalFileId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereReportDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereTotalCholesterol($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereTriglycerides($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|LipidProfile whereVldlCholesterol($value)
  *
  * @mixin \Eloquent
  */
