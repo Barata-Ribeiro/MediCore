316471a330f6472bb1a2361f2f69f7fa58e10bff
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-02 10:05:05 -0300

    refactor(UI): enhance EmptyChartData component with improved styling and icon integration

--- resources/js/components/common/empty-chart-data.tsx
+++ resources/js/components/common/empty-chart-data.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from '@/components/ui/empty';
 import type { RouteDefinition } from '@/wayfinder';
 import { Link } from '@inertiajs/react';
-import { ChartLineIcon } from 'lucide-react';
+import { ChartLineIcon, ClipboardPlusIcon } from 'lucide-react';
 
 type Props = {
     createRoute: RouteDefinition<'get'>;
@@ -10,7 +10,7 @@ type Props = {
 
 export function EmptyChartData({ createRoute }: Readonly<Props>) {
     return (
-        <Empty className="border border-dashed">
+        <Empty className="mx-auto h-full max-w-3xl rounded-4xl border border-dashed bg-muted/30">
             <EmptyHeader>
                 <EmptyMedia variant="icon">
                     <ChartLineIcon aria-hidden />
@@ -21,16 +21,15 @@ export function EmptyChartData({ createRoute }: Readonly<Props>) {
                 </EmptyDescription>
             </EmptyHeader>
             <EmptyContent>
-                <Button variant="outline" size="sm" asChild>
-                    <Link
-                        href={createRoute}
-                        aria-label="Create new record of this type"
-                        title="Create new record of this type"
-                        as="button"
-                        prefetch
-                        viewTransition
-                    >
-                        Create
+                <Button
+                    variant="outline"
+                    size="sm"
+                    aria-label="Create new record of this type"
+                    title="Create new record of this type"
+                    asChild
+                >
+                    <Link href={createRoute} as="button" prefetch viewTransition>
+                        <ClipboardPlusIcon aria-hidden size={16} /> Create
                     </Link>
                 </Button>
             </EmptyContent>
