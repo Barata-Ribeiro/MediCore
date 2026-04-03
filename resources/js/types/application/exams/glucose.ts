a7f8184d88f060425e8f85e3a8c03f9b0eb68988
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-03 09:17:51 -0300

    feat: add Glucose interface to define glucose data structure

---
+++ resources/js/types/application/exams/glucose.ts
@@ -0,0 +1,10 @@
+export interface Glucose {
+    id: number;
+    medical_file_id: number;
+    glucose_level: number;
+    glycated_hemoglobin: number;
+    estimated_average_glucose: number;
+    report_date: string;
+    created_at: string;
+    updated_at: string;
+}
