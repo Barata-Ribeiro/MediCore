ec321b6a22fd87c6bfb423b4a4320c1e17847fc2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-03 07:21:15 -0300

    feat(controller): add GlucoseController and define resource route

--- routes/exams.php
+++ routes/exams.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Exams\CompleteBloodCountController;
+use App\Http\Controllers\Exams\GlucoseController;
 use App\Http\Controllers\Exams\LipidProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('exams')->group(function () {
     Route::resource('lipid-profile', LipidProfileController::class);
     Route::resource('complete-blood-count', CompleteBloodCountController::class);
+    Route::resource('glucose', GlucoseController::class);
 });
