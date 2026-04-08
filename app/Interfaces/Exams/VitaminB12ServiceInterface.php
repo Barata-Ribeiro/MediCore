f7c178e6bb2a2968f59cd36aea26079311b240e8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-08 14:12:48 -0300

    feat(service): implement getVitaminB12sData method in VitaminB12Service for fetching paginated and chart data

--- app/Interfaces/Exams/VitaminB12ServiceInterface.php
+++ app/Interfaces/Exams/VitaminB12ServiceInterface.php
@@ -2,4 +2,17 @@
 
 namespace App\Interfaces\Exams;
 
-interface VitaminB12ServiceInterface {}
+use Illuminate\Pagination\LengthAwarePaginator;
+
+interface VitaminB12ServiceInterface
+{
+    /**
+     * Fetch paginated data and chart data for this exam type based on the provided parameters.
+     *
+     * @return array{
+     *     0: LengthAwarePaginator,
+     *     1: array<string, mixed>
+     * }
+     */
+    public function getVitaminB12sData(?int $perPage, ?string $sortBy, ?string $sortDir, ?string $search, ?array $filters): array;
+}
