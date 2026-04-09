b47f415e0327582d6d087725386daf4e312a39ab
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-09 10:12:20 -0300

    feat: add Vitamin B12 form component with input fields for level and report date

---
+++ resources/js/components/forms/exams/vitamin-b12.form.tsx
@@ -0,0 +1,75 @@
+import VitaminB12Controller from '@/actions/App/Http/Controllers/Exams/VitaminB12Controller';
+import DatePicker from '@/components/helpers/date-picker';
+import InputError from '@/components/helpers/input-error';
+import RequiredIndicator from '@/components/helpers/required-indicator';
+import { Button } from '@/components/ui/button';
+import { Field, FieldLabel } from '@/components/ui/field';
+import { InputGroup, InputGroupAddon, InputGroupInput, InputGroupText } from '@/components/ui/input-group';
+import { Spinner } from '@/components/ui/spinner';
+import type { VitaminB12 } from '@/types/application/exams/vitamin-b12';
+import { Form } from '@inertiajs/react';
+import { Activity, Fragment, memo } from 'react';
+
+type Props = {
+    vitaminB12?: VitaminB12;
+};
+
+const VitaminB12Form = memo<Readonly<Props>>(({ vitaminB12 }) => {
+    const isEditMode = vitaminB12 && vitaminB12 !== null;
+
+    const formRoute = isEditMode ? VitaminB12Controller.update.form(vitaminB12.id) : VitaminB12Controller.store.form();
+
+    return (
+        <Form
+            {...formRoute}
+            options={{ preserveScroll: true }}
+            disableWhileProcessing
+            className="space-y-6 inert:pointer-events-none inert:grayscale-100"
+        >
+            {({ processing, errors }) => (
+                <Fragment>
+                    <Field data-invalid={!!errors['vitamin_b12_level']}>
+                        <FieldLabel htmlFor="vitamin_b12_level">
+                            Vitamin B12 <RequiredIndicator />
+                        </FieldLabel>
+                        <InputGroup>
+                            <InputGroupInput
+                                type="number"
+                                id="vitamin_b12_level"
+                                name="vitamin_b12_level"
+                                placeholder="e.g. 500"
+                                defaultValue={vitaminB12?.vitamin_b12_level}
+                                min={0}
+                                step={0.01}
+                                aria-invalid={!!errors['vitamin_b12_level']}
+                                required
+                                aria-required
+                            />
+                            <InputGroupAddon align="inline-end">
+                                <InputGroupText>pg/mL</InputGroupText>
+                            </InputGroupAddon>
+                        </InputGroup>
+                        <InputError message={errors['vitamin_b12_level']} />
+                    </Field>
+
+                    <Field data-invalid={!!errors['report_date']}>
+                        <FieldLabel htmlFor="report_date">
+                            Report Date <RequiredIndicator />
+                        </FieldLabel>
+                        <DatePicker id="report_date" name="report_date" defaultValue={vitaminB12?.report_date} />
+                        <InputError message={errors['report_date']} />
+                    </Field>
+
+                    <Button data-test="save-vitamin-b12" type="submit" disabled={processing}>
+                        <Activity mode={processing ? 'visible' : 'hidden'}>
+                            <Spinner aria-hidden />
+                        </Activity>
+                        Save
+                    </Button>
+                </Fragment>
+            )}
+        </Form>
+    );
+});
+
+export default VitaminB12Form;
