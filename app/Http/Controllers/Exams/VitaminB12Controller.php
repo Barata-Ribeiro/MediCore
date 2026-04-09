600dd13ca6879ff0a165e633a994fbb55173b8c8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-09 07:21:48 -0300

    feat(vitamin-b12): implement create and store methods for Vitamin B12 records with validation and error handling

--- app/Http/Controllers/Exams/VitaminB12Controller.php
+++ app/Http/Controllers/Exams/VitaminB12Controller.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Exams;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Exams\VitaminB12Request;
 use App\Http\Requests\QueryRequest;
 use App\Interfaces\Exams\VitaminB12ServiceInterface;
+use Exception;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Inertia\Inertia;
+use Log;
 
 use function in_array;
 
@@ -22,6 +25,32 @@ class VitaminB12Controller extends Controller
             'vitaminB12s' => $vitaminB12s,
             'chartData' => $chartData,
         ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('exams/vitamin-b12/create');
+    }
+
+    public function store(VitaminB12Request $request)
+    {
+        $user = $request->user();
+
+        $validated = $request->validated();
+
+        try {
+
+            $user->medicalFile->vitaminB12s()->create($validated);
+
+            Inertia::flash('success', 'Vitamin B12 record created successfully.');
+
+            return to_route('vitamin-b12.index');
+        } catch (Exception $e) {
+            Inertia::flash('error', 'An error occurred while creating the Vitamin B12 record.');
+            Log::error('Error creating Vitamin B12 record', ['user_id' => $request->user()->id, 'error' => $e->getMessage()]);
+
+            return back()->withInput();
+        }
     }
 
     /**
