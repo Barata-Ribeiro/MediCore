16569e1f41de35847a4ba4b7a5d3c13d6d6f7e89
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-10 14:38:36 -0300

    feat(ui): adjust layout of AppSidebarHeader for improved appearance

--- resources/js/components/application/app-sidebar-header.tsx
+++ resources/js/components/application/app-sidebar-header.tsx
@@ -1,13 +1,20 @@
+import AppearanceMenu from '@/components/helpers/appearance-menu';
 import { Breadcrumbs } from '@/components/helpers/breadcrumbs';
+import { Separator } from '@/components/ui/separator';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import type { BreadcrumbItem as BreadcrumbItemType } from '@/types';
 
 export function AppSidebarHeader({ breadcrumbs = [] }: Readonly<{ breadcrumbs?: BreadcrumbItemType[] }>) {
     return (
         <header className="flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
-            <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1" />
+            <div className="inline-flex items-center gap-x-2">
+                <SidebarTrigger />
+                <Separator orientation="vertical" />
                 <Breadcrumbs breadcrumbs={breadcrumbs} />
+            </div>
+
+            <div className="ml-auto inline-flex items-center gap-x-2">
+                <AppearanceMenu />
             </div>
         </header>
     );
