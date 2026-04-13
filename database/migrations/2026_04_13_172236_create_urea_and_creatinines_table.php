3caeffebc2830ff215ffb2ed8452dd1fdc364e89
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-13 14:29:18 -0300

    feat(migrations): create urea and creatinine table with necessary fields and constraints

---
+++ database/migrations/2026_04_13_172236_create_urea_and_creatinines_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('urea_and_creatinines', function (Blueprint $table) {
+            $table->id();
+            $table->double('urea_level')->comment('Urea level');
+            $table->double('creatinine_level')->comment('Creatinine level');
+            $table->date('report_date')->comment('Date of the Urea and Creatinine report');
+            $table->timestamps();
+
+            $table->foreignId('medical_file_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->index('medical_file_id');
+            $table->index(['report_date', 'medical_file_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('urea_and_creatinines');
+    }
+};
