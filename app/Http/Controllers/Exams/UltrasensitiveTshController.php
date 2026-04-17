f0f3f9f7beae8ed46c78dcce44f8a1b5d7e8ee0e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-17 06:45:21 -0300

    feat(store): implement store method for creating Ultrasensitive TSH records with error handling

--- app/Http/Controllers/Exams/UltrasensitiveTshController.php
+++ app/Http/Controllers/Exams/UltrasensitiveTshController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Exams;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Exams\UltrasensitiveTshRequest;
 use App\Http\Requests\QueryRequest;
 use App\Interfaces\Exams\UltrasensitiveTshServiceInterface;
+use Exception;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Inertia\Inertia;
+use Log;
 
 use function in_array;
 
@@ -22,6 +25,32 @@ class UltrasensitiveTshController extends Controller
             'ultrasensitiveTshs' => $ultrasensitiveTshs,
             'chartData' => $chartData,
         ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('exams/ultrasensitive-tsh/create');
+    }
+
+    public function store(UltrasensitiveTshRequest $request)
+    {
+        $user = $request->user();
+
+        $validated = $request->validated();
+
+        try {
+
+            $user->medicalFile->ultrasensitiveTshs()->create($validated);
+
+            Inertia::flash('toast', ['type' => 'success', 'message' => 'Ultrasensitive TSH record created successfully.']);
+
+            return to_route('ultrasensitive-tsh.index');
+        } catch (Exception $e) {
+            Inertia::flash('toast', ['type' => 'error', 'message' => 'An error occurred while creating the Ultrasensitive TSH record.']);
+            Log::error('Error creating Ultrasensitive TSH record', ['user_id' => $request->user()->id, 'error' => $e->getMessage()]);
+
+            return back()->withInput();
+        }
     }
 
     /**
