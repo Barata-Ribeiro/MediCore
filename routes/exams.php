0dfcfc3bea328d0fe3383a985f7c0eb1533b3bb2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-20 07:17:27 -0300

    feat(controller): add UricAcidController and define resource route

--- routes/exams.php
+++ routes/exams.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Exams\UltrasensitiveTshController;
 use App\Http\Controllers\Exams\UreaAndCreatinineController;
 use App\Http\Controllers\Exams\VitaminB12Controller;
 use App\Http\Controllers\Exams\VitaminD3Controller;
+use App\Http\Controllers\UricAcidController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('exams')->group(function () {
@@ -15,6 +16,7 @@ Route::middleware(['auth', 'verified'])->prefix('exams')->group(function () {
     Route::resource('lipid-profile', LipidProfileController::class);
     Route::resource('ultrasensitive-tsh', UltrasensitiveTshController::class);
     Route::resource('urea-and-creatinine', UreaAndCreatinineController::class);
+    Route::resource('uric-acid', UricAcidController::class);
     Route::resource('vitamin-b12', VitaminB12Controller::class);
     Route::resource('vitamin-d3', VitaminD3Controller::class);
 });
