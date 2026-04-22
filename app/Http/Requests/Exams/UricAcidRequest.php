3054ba12828464609b9e9c4287e088f329466a11
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-22 07:42:28 -0300

    feat(uric-acid): add UricAcidRequest with validation rules for report date and uric acid level

---
+++ app/Http/Requests/Exams/UricAcidRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\Exams;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UricAcidRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'report_date' => ['required', 'date'],
+            'uric_acid_level' => ['required', 'numeric', 'min:0'],
+        ];
+    }
+}
