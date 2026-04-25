48a16b90503d8202ea4a073e8e201c65e6e225b5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-25 08:00:15 -0300

    refactor(profile): enhance layout structure and set breadcrumbs for profile settings

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -5,7 +5,7 @@ import BaseAccountUpdateForm from '@/components/forms/settings/base-account-upda
 import PersonalProfileManagerForm from '@/components/forms/settings/personal-profile-manager.form';
 import DeleteUser from '@/components/helpers/delete-user';
 import { Separator } from '@/components/ui/separator';
-import { Head } from '@inertiajs/react';
+import { Head, setLayoutProps } from '@inertiajs/react';
 import { Fragment } from 'react';
 
 export default function Profile({
@@ -15,13 +15,25 @@ export default function Profile({
     mustVerifyEmail: boolean;
     status?: string;
 }>) {
+    setLayoutProps({
+        breadcrumbs: [
+            {
+                title: 'Profile settings',
+                href: edit(),
+            },
+        ],
+    });
+
     return (
         <Fragment>
             <Head title="Profile settings" />
-
             <h1 className="sr-only">Profile settings</h1>
 
-            <BaseAccountUpdateForm mustVerifyEmail={mustVerifyEmail} status={status} />
+            <div className="space-y-6">
+                <Heading variant="small" title="Profile information" description="Update your name and email address" />
+
+                <BaseAccountUpdateForm mustVerifyEmail={mustVerifyEmail} status={status} />
+            </div>
 
             <Separator className="my-6" />
 
@@ -33,18 +45,13 @@ export default function Profile({
 
             <Separator className="mb-6" />
 
-            <PersonalProfileManagerForm />
+            <div className="space-y-6">
+                <Heading variant="small" title="Personal Information" description="Update your personal details" />
+
+                <PersonalProfileManagerForm />
+            </div>
 
             <DeleteUser />
         </Fragment>
     );
 }
-
-Profile.layout = {
-    breadcrumbs: [
-        {
-            title: 'Profile settings',
-            href: edit(),
-        },
-    ],
-};
