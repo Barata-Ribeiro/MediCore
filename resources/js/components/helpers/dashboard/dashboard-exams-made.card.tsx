e5c8307d466cc14f1175e7a20bc18623ad4d7e11
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-27 07:32:14 -0300

    feat(translations): implement translations for dashboard exams made card content

--- resources/js/components/helpers/dashboard/dashboard-exams-made.card.tsx
+++ resources/js/components/helpers/dashboard/dashboard-exams-made.card.tsx
@@ -1,5 +1,6 @@
 import ExamsSummaryChart from '@/components/application/charts/exams-summary.chart';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { lang } from '@erag/lang-sync-inertia/react';
 import { memo } from 'react';
 
 type Props = {
@@ -7,11 +8,13 @@ type Props = {
 };
 
 const DashboardExamsMadeCard = memo<Readonly<Props>>(({ exams }) => {
+    const { __ } = lang();
+
     return (
         <Card className="w-full">
             <CardHeader>
-                <CardTitle className="text-xl">Exams Made</CardTitle>
-                <CardDescription>A chart showing the number all exams made by you.</CardDescription>
+                <CardTitle className="text-xl">{__('dashboard.exams_made_card.title')}</CardTitle>
+                <CardDescription>{__('dashboard.exams_made_card.description')}</CardDescription>
             </CardHeader>
 
             <CardContent className="flex-1 pb-0">
@@ -19,7 +22,7 @@ const DashboardExamsMadeCard = memo<Readonly<Props>>(({ exams }) => {
             </CardContent>
 
             <CardFooter className="mt-auto border-t text-sm text-muted-foreground">
-                {(exams['total'] ?? 0).toLocaleString()} exams made
+                {(exams['total'] ?? 0).toLocaleString()} {__('dashboard.exams_made_card.made')}
             </CardFooter>
         </Card>
     );
