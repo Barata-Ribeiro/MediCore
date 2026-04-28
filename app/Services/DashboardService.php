add23a6c4263b4c13cb02b2b03a828309297d8c9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-28 14:21:40 -0300

    refactor(DashboardService): streamline user data fetching in getDashboardData method

--- app/Services/DashboardService.php
+++ app/Services/DashboardService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use App\Interfaces\DashboardServiceInterface;
-use App\Models\User;
 
 class DashboardService implements DashboardServiceInterface
 {
@@ -12,17 +11,18 @@ class DashboardService implements DashboardServiceInterface
      */
     public function getDashboardData(): array
     {
-        $data = User::query()
-            ->with(['profile', 'medicalFile' => fn ($q) => $q->selectRaw('medical_files.*,
+        $data = auth()->user()
+            ->with([
+                'profile',
+                'medicalFile' => fn ($q) => $q->selectRaw('medical_files.*,
                 (SELECT COUNT(*) FROM complete_blood_counts WHERE medical_file_id = medical_files.id) AS complete_blood_counts_count,
                 (SELECT COUNT(*) FROM lipid_profiles WHERE medical_file_id = medical_files.id) AS lipid_profiles_count,
                 (SELECT COUNT(*) FROM glucoses WHERE medical_file_id = medical_files.id) AS glucoses_count,
                 (SELECT COUNT(*) FROM ultrasensitive_tshs WHERE medical_file_id = medical_files.id) AS ultrasensitive_tshs_count,
                 (SELECT COUNT(*) FROM urea_and_creatinines WHERE medical_file_id = medical_files.id) AS urea_and_creatinines_count,
                 (SELECT COUNT(*) FROM vitamin_d3_s WHERE medical_file_id = medical_files.id) AS vitamin_d3s_count,
-                (SELECT COUNT(*) FROM vitamin_b12_s WHERE medical_file_id = medical_files.id) AS vitamin_b12s_count')])
-            ->where('id', auth()->id())
-            ->first();
+                (SELECT COUNT(*) FROM vitamin_b12_s WHERE medical_file_id = medical_files.id) AS vitamin_b12s_count'),
+            ]);
 
         $medicalFile = $data->medicalFile;
         $completeBloodCountCount = $medicalFile?->complete_blood_counts_count ?? 0;
