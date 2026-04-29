1222968efeeaf517de58d03764070e25ec95b4cd
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-29 08:04:08 -0300

    docs(Profile): add teams property and methods for team management

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -375,6 +375,9 @@ namespace App\Models{
  * @property-read Collection<int, Role> $roles
  * @property-read int|null $roles_count
  * @property-read bool|null $roles_exists
+ * @property-read Collection<int, Permission> $teams
+ * @property-read int|null $teams_count
+ * @property-read bool|null $teams_exists
  * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
  * @method static Builder<static>|User isSuperAdmin()
  * @method static Builder<static>|User newModelQuery()
@@ -382,6 +385,7 @@ namespace App\Models{
  * @method static Builder<static>|User permission($permissions, bool $without = false)
  * @method static Builder<static>|User query()
  * @method static Builder<static>|User role($roles, ?string $guard = null, bool $without = false)
+ * @method static Builder<static>|User team($teams, bool $without = false)
  * @method static Builder<static>|User whereAvatar($value)
  * @method static Builder<static>|User whereCreatedAt($value)
  * @method static Builder<static>|User whereEmail($value)
@@ -402,6 +406,7 @@ namespace App\Models{
  * @method static Builder<static>|User whereUpdatedAt($value)
  * @method static Builder<static>|User withoutPermission($permissions)
  * @method static Builder<static>|User withoutRole($roles, ?string $guard = null)
+ * @method static Builder<static>|User withoutTeam($teams)
  * @mixin \Eloquent
  */
 	class User extends \Eloquent {}
