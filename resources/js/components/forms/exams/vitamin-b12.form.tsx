84915c528ea7a5fdc184e0c861a30b606776bb65
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-04-29 07:38:00 -0300

    feat(translations): integrate translations for Vitamin B12 chart and form components

--- resources/js/components/forms/exams/vitamin-b12.form.tsx
+++ resources/js/components/forms/exams/vitamin-b12.form.tsx
@@ -7,6 +7,7 @@ import { Field, FieldLabel } from '@/components/ui/field';
 import { InputGroup, InputGroupAddon, InputGroupInput, InputGroupText } from '@/components/ui/input-group';
 import { Spinner } from '@/components/ui/spinner';
 import type { VitaminB12 } from '@/types/application/exams/vitamin-b12';
+import { lang } from '@erag/lang-sync-inertia/react';
 import { Form } from '@inertiajs/react';
 import { Activity, Fragment, memo } from 'react';
 
@@ -15,6 +16,7 @@ type Props = {
 };
 
 const VitaminB12Form = memo<Readonly<Props>>(({ vitaminB12 }) => {
+    const { __ } = lang();
     const isEditMode = vitaminB12 && vitaminB12 !== null;
 
     const formRoute = isEditMode ? VitaminB12Controller.update.form(vitaminB12.id) : VitaminB12Controller.store.form();
@@ -30,14 +32,14 @@ const VitaminB12Form = memo<Readonly<Props>>(({ vitaminB12 }) => {
                 <Fragment>
                     <Field data-invalid={!!errors['vitamin_b12_level']}>
                         <FieldLabel htmlFor="vitamin_b12_level">
-                            Vitamin B12 <RequiredIndicator />
+                            {__('vitamin_b12_pages.form.vitamin_b12')} <RequiredIndicator />
                         </FieldLabel>
                         <InputGroup>
                             <InputGroupInput
                                 type="number"
                                 id="vitamin_b12_level"
                                 name="vitamin_b12_level"
-                                placeholder="e.g. 500"
+                                placeholder={__('vitamin_b12_pages.form.vitamin_b12_placeholder')}
                                 defaultValue={vitaminB12?.vitamin_b12_level}
                                 min={0}
                                 step={0.01}
@@ -46,7 +48,7 @@ const VitaminB12Form = memo<Readonly<Props>>(({ vitaminB12 }) => {
                                 aria-required
                             />
                             <InputGroupAddon align="inline-end">
-                                <InputGroupText>pg/mL</InputGroupText>
+                                <InputGroupText>{__('vitamin_b12_pages.shared.unit')}</InputGroupText>
                             </InputGroupAddon>
                         </InputGroup>
                         <InputError message={errors['vitamin_b12_level']} />
@@ -54,7 +56,7 @@ const VitaminB12Form = memo<Readonly<Props>>(({ vitaminB12 }) => {
 
                     <Field data-invalid={!!errors['report_date']}>
                         <FieldLabel htmlFor="report_date">
-                            Report Date <RequiredIndicator />
+                            {__('vitamin_b12_pages.form.report_date')} <RequiredIndicator />
                         </FieldLabel>
                         <DatePicker id="report_date" name="report_date" defaultValue={vitaminB12?.report_date} />
                         <InputError message={errors['report_date']} />
@@ -64,7 +66,7 @@ const VitaminB12Form = memo<Readonly<Props>>(({ vitaminB12 }) => {
                         <Activity mode={processing ? 'visible' : 'hidden'}>
                             <Spinner aria-hidden />
                         </Activity>
-                        Save
+                        {__('vitamin_b12_pages.form.submit')}
                     </Button>
                 </Fragment>
             )}
