c4b326404220daa7206137b191de54e8c6d3a15f
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-07-03 13:47:58 -0300

    feat: create migration for total proteins and fractions table

---
+++ database/migrations/2026_07_03_163651_create_total_proteins_and_fractions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('total_proteins_and_fractions', function (Blueprint $table) {
+            $table->id();
+            $table->double('total_proteins')->comment('Total Proteins level');
+            $table->double('albumin')->comment('Albumin level');
+            $table->double('globulin')->comment('Globulin level');
+            $table->date('report_date')->comment('Date of the Vitamin B12 report');
+            $table->timestamps();
+
+            $table->foreignId('medical_file_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->index('medical_file_id');
+            $table->index(['report_date', 'medical_file_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('total_proteins_and_fractions');
+    }
+};
