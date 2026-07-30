73d0df0b71dcbf64ab50c530e6f6cb3b2829c5ae
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-07-30 08:18:33 -0300

    feat(routes): add routes for exercises and muscle groups management

--- routes/workout.php
+++ routes/workout.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Fitness\ExerciseController;
+use App\Http\Controllers\Fitness\MuscleGroupController;
 use App\Http\Controllers\Fitness\WorkoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('fitness')->group(function () {
     Route::resource('workouts', WorkoutController::class);
+    Route::resource('exercises', ExerciseController::class)->only(['index', 'store', 'update', 'destroy']);
+    Route::resource('muscle-groups', MuscleGroupController::class)->only(['index', 'store', 'update', 'destroy']);
 });
