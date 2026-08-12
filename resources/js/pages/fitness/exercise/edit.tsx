a711051e2259d07ce30992e02a9a08935c8db57d
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-12 10:56:10 -0300

    feat(exercise): add CreateExercise and EditExercise components with modal support

---
+++ resources/js/pages/fitness/exercise/edit.tsx
@@ -0,0 +1,42 @@
+import ExerciseRegistryForm from '@/components/forms/fitness/exercise-registry.form';
+import { Button } from '@/components/ui/button';
+import { CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import type { CatalogExercise, CatalogMuscleGroup } from '@/types/application/fitness/catalog';
+import { lang } from '@erag/lang-sync-inertia/react';
+import { Modal } from '@inertiaui/modal-react';
+import { XIcon } from 'lucide-react';
+import { Fragment } from 'react/jsx-runtime';
+
+type CloseActionType = {
+    close: () => void;
+};
+
+type Props = {
+    exercise: CatalogExercise;
+    muscleGroups: CatalogMuscleGroup[];
+};
+
+export default function EditExercise({ exercise, muscleGroups }: Readonly<Props>) {
+    const { __ } = lang();
+
+    return (
+        <Modal>
+            {({ close }: Readonly<CloseActionType>) => (
+                <Fragment>
+                    <CardHeader className="flex items-start justify-between gap-2 border-b">
+                        <div className="w-max space-y-1">
+                            <CardTitle>{__('exercise_pages.form.title')}</CardTitle>
+                            <CardDescription>{__('exercise_pages.form.description')}</CardDescription>
+                        </div>
+                        <Button onClick={close} variant="secondary" size="icon">
+                            <XIcon aria-hidden />
+                        </Button>
+                    </CardHeader>
+                    <CardContent>
+                        <ExerciseRegistryForm exercise={exercise} muscleGroups={muscleGroups} closeAction={close} />
+                    </CardContent>
+                </Fragment>
+            )}
+        </Modal>
+    );
+}
