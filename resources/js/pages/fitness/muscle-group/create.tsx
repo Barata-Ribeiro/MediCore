19804c5a4bfcf3d36722a501fafbf4f428eaee56
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-13 10:29:18 -0300

    feat(muscle-group): implement CreateMuscleGroup component with modal support

---
+++ resources/js/pages/fitness/muscle-group/create.tsx
@@ -0,0 +1,42 @@
+import MuscleGroupRegistryForm from '@/components/forms/fitness/muscle-group-registry.form';
+import { Button } from '@/components/ui/button';
+import { CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { lang } from '@erag/lang-sync-inertia/react';
+import { Modal } from '@inertiaui/modal-react';
+import { XIcon } from 'lucide-react';
+import { Fragment } from 'react/jsx-runtime';
+
+type CloseActionType = {
+    close: () => void;
+};
+
+export default function CreateMuscleGroup() {
+    const { __ } = lang();
+
+    return (
+        <Modal>
+            {({ close }: Readonly<CloseActionType>) => (
+                <Fragment>
+                    <CardHeader className="flex items-start justify-between gap-2 border-b">
+                        <div className="w-max space-y-1">
+                            <CardTitle>{__('muscle_group_pages.form.title')}</CardTitle>
+                            <CardDescription>{__('muscle_group_pages.form.description')}</CardDescription>
+                        </div>
+                        <Button
+                            onClick={close}
+                            variant="secondary"
+                            size="icon"
+                            aria-label={__('muscle_group_pages.form.cancel_action')}
+                            title={__('muscle_group_pages.form.cancel_action')}
+                        >
+                            <XIcon aria-hidden />
+                        </Button>
+                    </CardHeader>
+                    <CardContent>
+                        <MuscleGroupRegistryForm closeAction={close} />
+                    </CardContent>
+                </Fragment>
+            )}
+        </Modal>
+    );
+}
