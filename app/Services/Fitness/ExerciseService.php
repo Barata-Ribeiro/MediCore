a203bcc06529096555b93e44c6b15fb1c90ee4db
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-15 09:47:24 -0300

    feat(exercise): implement getExercisesData method with pagination, sorting, searching, and filtering

--- app/Services/Fitness/ExerciseService.php
+++ app/Services/Fitness/ExerciseService.php
@@ -2,9 +2,51 @@
 
 namespace App\Services\Fitness;
 
+use App\Common\Helpers;
 use App\Interfaces\Fitness\ExerciseServiceInterface;
+use App\Models\Fitness\Exercise;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
+
+use function in_array;
 
 class ExerciseService implements ExerciseServiceInterface
 {
+    private bool $isSqlDriver;
 
+    public function __construct()
+    {
+        $this->isSqlDriver = in_array(DB::getDriverName(), ['mysql', 'pgsql']);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getExercisesData(?int $perPage, ?string $sortBy, ?string $sortDir, ?string $search, $filters): LengthAwarePaginator
+    {
+        $isSql = $this->isSqlDriver;
+
+        $filters ??= [];
+        $createdAtRange = $filters['created_at'] ?? [];
+        $reportDateRange = $filters['report_date'] ?? [];
+
+        [$createdAtStart, $createdAtEnd] = Helpers::getDateRange($createdAtRange);
+        [$reportDateStart, $reportDateEnd] = Helpers::getDateRange($reportDateRange);
+
+        return Exercise::query()
+            ->whereBelongsTo(auth()->user())
+            ->with(['muscleGroups' => fn ($query) => $query->select(['muscle_groups.id', 'name'])->orderBy('name')])
+            ->when($createdAtRange, fn ($q) => $q->whereBetween('created_at', [$createdAtStart, $createdAtEnd]))
+            ->when($reportDateRange, fn ($q) => $q->whereBetween('report_date', [$reportDateStart, $reportDateEnd]))
+            ->when($search, function ($qr) use ($search, $isSql) {
+                if ($isSql) {
+                    $booleanQuery = Helpers::buildBooleanQuery($search);
+                    $qr->whereFullText(['exercises.name', 'exercises.description', 'exercises.video_url'], $booleanQuery);
+                } else {
+                    $qr->whereLike('exercises.name', "%{$search}%");
+                }
+            })->orderBy($sortBy, $sortDir)
+            ->paginate($perPage)
+            ->withQueryString();
+    }
 }
