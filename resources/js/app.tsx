e9f3cbfd4aef21eab9aefc31d2a6a68fbdc00f55
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-20 07:03:24 -0300

    refactor(app): update panelClasses syntax for improved readability

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -63,9 +63,7 @@ putConfig({
         closeOnClickOutside: true,
         maxWidth: 'full',
         paddingClasses: 'py-(--card-spacing)',
-        panelClasses: cn(
-            'group/card flex flex-col gap-(--card-spacing) overflow-hidden rounded-[min(var(--radius-4xl),24px)] bg-card text-sm text-card-foreground shadow-sm ring-1 ring-foreground/5 [--card-spacing:--spacing(5)] has-[>img:first-child]:pt-0 dark:ring-foreground/10 *:[img:first-child]:rounded-t-[min(var(--radius-4xl),24px)] *:[img:last-child]:rounded-b-[min(var(--radius-4xl),24px)]',
-        ),
+        panelClasses: cn`group/card flex flex-col gap-(--card-spacing) overflow-hidden rounded-[min(var(--radius-4xl),24px)] bg-card text-sm text-card-foreground shadow-sm ring-1 ring-foreground/5 [--card-spacing:--spacing(5)] has-[>img:first-child]:pt-0 dark:ring-foreground/10 *:[img:first-child]:rounded-t-[min(var(--radius-4xl),24px)] *:[img:last-child]:rounded-b-[min(var(--radius-4xl),24px)]`,
         position: 'center',
     },
     slideover: {
