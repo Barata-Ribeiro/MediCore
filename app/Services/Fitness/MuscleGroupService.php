0e3aad8019775dd7e9b461f81c85e982700698fc
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-08-24 09:47:42 -0300

    feat(fitness): implement getMuscleGroupsData method with pagination, sorting, searching, and filtering

--- app/Services/Fitness/MuscleGroupService.php
+++ app/Services/Fitness/MuscleGroupService.php
@@ -2,6 +2,32 @@
 
 namespace App\Services\Fitness;
 
+use App\Common\Helpers;
 use App\Interfaces\Fitness\MuscleGroupServiceInterface;
+use App\Models\Fitness\MuscleGroup;
+use Illuminate\Pagination\LengthAwarePaginator;
 
-class MuscleGroupService implements MuscleGroupServiceInterface {}
+class MuscleGroupService implements MuscleGroupServiceInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getMuscleGroupsData(?int $perPage, ?string $sortBy, ?string $sortDir, ?string $search, $filters): LengthAwarePaginator
+    {
+        $filters ??= [];
+        $createdAtRange = $filters['created_at'] ?? [];
+        $exercisesCountRange = $filters['exercises_count'] ?? [];
+
+        [$createdAtStart, $createdAtEnd] = Helpers::getDateRange($createdAtRange);
+
+        return MuscleGroup::query()
+            ->whereBelongsTo(auth()->user())
+            ->withCount('exercises')
+            ->when($createdAtRange, fn ($q) => $q->whereBetween('created_at', [$createdAtStart, $createdAtEnd]))
+            ->when($exercisesCountRange, fn ($q) => $q->whereBetween('exercises_count', [$exercisesCountRange[0] ?? 0, $exercisesCountRange[1] ?? PHP_INT_MAX]))
+            ->when($search, fn ($q) => $q->whereLike('name', "%{$search}%"))
+            ->orderBy($sortBy, $sortDir)
+            ->paginate($perPage)
+            ->withQueryString();
+    }
+}
