a99f57b26d959934cc5ba926e40866b656cd8882
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2026-09-24 07:48:56 -0300

    refact: update DateCell to use date-fns for formatting and improve locale handling

--- resources/js/components/table/cell-components.tsx
+++ resources/js/components/table/cell-components.tsx
@@ -1,5 +1,8 @@
 import { useCellContext } from '@/hooks/table-context';
+import { tz } from '@date-fns/tz';
 import { usePage } from '@inertiajs/react';
+import { format } from 'date-fns';
+import { enUS, ptBR } from 'date-fns/locale';
 
 export function TextCell() {
     const cell = useCellContext<string | number | boolean | null>();
@@ -10,13 +13,12 @@ export function DateCell() {
     const cell = useCellContext<string>();
     const { auth } = usePage().props;
     const value = cell.getValue();
+
+    const locale = auth.locale === 'pt_BR' ? ptBR : enUS;
+
     return (
-        <span>
-            {value
-                ? new Intl.DateTimeFormat((auth.locale ?? 'en').replace('_', '-'), { timeZone: 'UTC' }).format(
-                      new Date(value),
-                  )
-                : '—'}
-        </span>
+        <time dateTime={value ?? undefined}>
+            {value ? format(new Date(value), 'PPP', { locale, in: tz('UTC') }) : '—'}
+        </time>
     );
 }
